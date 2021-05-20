1fcc2ddeaa01382694d0f1834143bd73a604acce
Alan Bruno Ferreira da Silva <alanjjr83@gmail.com>
2021-05-20 17:28:30 -0300

    First Structure and Component Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,33 @@
+/* eslint-disable jsx-a11y/alt-text */
+
+import './styles.css'
+
+import logoJussi from './assets/logoJussi.svg';
+import search from './assets/search.svg';
+import cart from './assets/cart.svg'
+
+export default function Header() {
+  return (
+    <nav className="nav-bar">
+      <div className="menu">
+        <img src={logoJussi} alt="Logo Jussi" />
+        <strong>Nossas Soluções</strong>
+        <strong>Conheça a Jüssi</strong>
+      </div>
+
+      <div className="search-login-cart">
+      
+        <div className="box"> 
+          <input className="search" type="text" placeholder="Buscar" />
+          <button id="submit" type="button"> 
+              <img id="botao"src={search} alt="" />
+          </button>
+        </div>
+            <p>Login</p>
+            <img src={cart} alt="" />
+      </div>
+
+    </nav>
+
+  )
+}
